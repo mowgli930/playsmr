2c4c12d5eb6e79a27b046ce2584354336f06d578
leo <leo.moralesvega@gmail.com>
2018-01-14 15:59:58 +0100

    Added spring boot generated project

---
+++ src/main/java/se/lemv/playsmr/PlaysmrApplication.java
@@ -0,0 +1,12 @@
+package se.lemv.playsmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaysmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaysmrApplication.class, args);
+	}
+}
---
+++ src/test/java/se/lemv/playsmr/PlaysmrApplicationTests.java
@@ -0,0 +1,16 @@
+package se.lemv.playsmr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlaysmrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
